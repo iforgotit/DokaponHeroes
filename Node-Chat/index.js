3c774ca13e48a762352d67152c36059d83b4a880
iforgotit <iforgotit4040@gmail.com>
2017-05-19 17:04:33 -0500

    init

---
+++ Node-Chat/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.use(express.static('public'));
+
+io.on('connection', function(socket){
+
+  console.log('a user connected');
+
+  socket.on('email', function(email){
+    console.log('email: ' + email);
+  });
+
+  socket.on('dname', function(dname){
+    console.log('Display Name: ' + dname);
+  });
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+    
